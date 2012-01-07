fd2a7749e2540f423fd32ce1681eca39a020c6ea
Florian <flohw@flohw-laptop.(none)>
2012-01-07 19:45:35 +0100

    vueconsultperio

--- src/VueConsulterPeriodique.java
+++ src/VueConsulterPeriodique.java
@@ -11,6 +11,7 @@ import java.awt.event.ActionEvent;
 import java.util.Observable;
 import javax.swing.SwingConstants;
 import java.awt.Font;
+import javax.swing.ListSelectionModel;
 
 
 public class VueConsulterPeriodique extends Vue {
@@ -21,14 +22,13 @@ public class VueConsulterPeriodique extends Vue {
 	private JLabel lblParutions;
 	private JLabel lblNomDuPriodique;
 	private JLabel lblDateDabonnement;
-	
-	private JTextField textFieldIssnRech;
 	private JTextField textFieldNom;
 	private JTextField textFieldDate;
 	
 	private JScrollPane scrollPane;
 	private JList listPeriodiques;
 	private DefaultListModel modelePeriodiques = new DefaultListModel();
+	private DefaultListModel modeleIssn = new DefaultListModel();	
 	
 	private JButton btnRechercher;
 	private JButton btnTerminer;
@@ -36,23 +36,25 @@ public class VueConsulterPeriodique extends Vue {
 	private JSeparator separator;
 	
 	private Periodique _periodique;
+	private JScrollPane scrollIssn;
+	private JList listIssn;
 
 	public VueConsulterPeriodique(Controleur controleur) {
 		super(controleur);
 		content = new JPanel();
 		getFrame().setTitle("Consulter un Périodique");
-		getFrame().setBounds(100, 100, 533, 400);
+		getFrame().setBounds(100, 100, 533, 460);
 		getFrame().setContentPane(content);
 		
 		lblIssn = new JLabel("ISSN");
 		lblIssn.setBounds(23, 23, 28, 16);
 		
-		textFieldIssnRech = new JTextField();
-		textFieldIssnRech.setBounds(219, 17, 166, 28);
-		textFieldIssnRech.setColumns(10);
+		listIssn = new JList(modeleIssn);
+		scrollIssn = new JScrollPane(listIssn);
+		scrollIssn.setBounds(219, 23, 291, 104);
 		
 		btnRechercher = new JButton("Rechercher");
-		btnRechercher.setBounds(397, 18, 113, 29);
+		btnRechercher.setBounds(23, 78, 113, 29);
 		btnRechercher.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				String issn = textFieldIssnRech.getText();
@@ -72,31 +74,31 @@ public class VueConsulterPeriodique extends Vue {
 		lblParutions = new JLabel("Parutions");
 		lblParutions.setFont(new Font("Lucida Grande", Font.BOLD | Font.ITALIC, 13));
 		lblParutions.setHorizontalAlignment(SwingConstants.CENTER);
-		lblParutions.setBounds(23, 164, 487, 16);
+		lblParutions.setBounds(23, 251, 487, 16);
 		
 		lblNomDuPriodique = new JLabel("Nom du périodique");
-		lblNomDuPriodique.setBounds(23, 90, 121, 16);
+		lblNomDuPriodique.setBounds(23, 177, 121, 16);
 		
 		textFieldNom = new JTextField();
-		textFieldNom.setBounds(219, 84, 291, 28);
+		textFieldNom.setBounds(219, 171, 291, 28);
 		textFieldNom.setEditable(false);
 		textFieldNom.setColumns(10);
 		
 		lblDateDabonnement = new JLabel("Date d'abonnement");
-		lblDateDabonnement.setBounds(23, 130, 122, 16);
+		lblDateDabonnement.setBounds(23, 217, 122, 16);
 		
 		textFieldDate = new JTextField();
-		textFieldDate.setBounds(219, 124, 291, 28);
+		textFieldDate.setBounds(219, 211, 291, 28);
 		textFieldDate.setEditable(false);
 		textFieldDate.setColumns(10);
 		
 		listPeriodiques = new JList();
 		scrollPane = new JScrollPane(listPeriodiques);
-		scrollPane.setBounds(23, 192, 487, 132);
+		scrollPane.setBounds(23, 279, 487, 132);
 		listPeriodiques.setModel(modelePeriodiques);
 		
 		btnTerminer = new JButton("Terminer");
-		btnTerminer.setBounds(216, 336, 100, 29);
+		btnTerminer.setBounds(216, 423, 100, 29);
 		btnTerminer.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				getControleur().fermerVue(VueConsulterPeriodique.this);
@@ -110,13 +112,13 @@ public class VueConsulterPeriodique extends Vue {
 		content.add(lblNomDuPriodique);
 		content.add(lblDateDabonnement);
 		content.add(lblParutions);
-		content.add(textFieldIssnRech);
 		content.add(textFieldNom);
 		content.add(textFieldDate);
 		content.add(btnRechercher);
 		content.add(btnTerminer);
 		content.add(separator);
-		
+		content.add(scrollIssn);
+				
 		getFrame().setVisible(true);
 	}
 	
