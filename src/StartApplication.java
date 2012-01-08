b23d76d2f8a0257c7551cc0a6cb966e5588d805e
Florian <floparreno@gmail.com>
2012-01-08 14:24:34 +0100

    restauration de la peur de ma vie ! (ou presque)

--- src/StartApplication.java
+++ src/StartApplication.java
@@ -31,8 +31,7 @@ public class StartApplication {
 	
 	public static void main(String args[]) {
 		try {
-			UIManager.setLookAndFeel(
-		            UIManager.getCrossPlatformLookAndFeelClassName());
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 			new StartApplication();
 		} catch (Exception e) {
 			e.printStackTrace();
